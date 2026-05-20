f176ad128438ce5d9a4a0a2566b175e614a1355f
polliard <polliard@users.noreply.github.com>
2026-05-20 14:33:04 +0000

    ai v0.4.0

--- Formula/ai.rb
+++ Formula/ai.rb
@@ -1,20 +1,20 @@
 class Ai < Formula
   desc "Polliard governance stack bootstrap binary"
   homepage "https://convergent-systems-co.github.io/ai/"
-  version "0.4.1"
+  version "0.4.0"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.1/ai-darwin-arm64"
-      sha256 "a807246f6b63cdce630b6118e9943ca8a800c38be7ac30c301b8fa3e610d8df3"
+      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.0/ai-darwin-arm64"
+      sha256 "9ab7f19b74427831c27fd5dbe2acdb86c0849a917a10b8c6e1855add020a0681"
 
       def install
         bin.install "ai-darwin-arm64" => "ai"
       end
     else
-      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.1/ai-darwin-amd64"
-      sha256 "0913493feb531bd1c08fcd1ae84f8196e41a08f92142419fd271db3f88830938"
+      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.0/ai-darwin-amd64"
+      sha256 "289157af4f5ec706239cb19ba8865ddd1f3051b533077bb4ccbfe916eae110eb"
 
       def install
         bin.install "ai-darwin-amd64" => "ai"
@@ -24,15 +24,15 @@ class Ai < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.1/ai-linux-arm64"
-      sha256 "6f4a2716f32a744a36e9fff2d8164082ea9bc4fce7506648cdfeb66598b284fa"
+      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.0/ai-linux-arm64"
+      sha256 "29bc4755e3be2c9e26d87b0ef8f74ba1fcf10f8675c0ac75085f7741bc5edbda"
 
       def install
         bin.install "ai-linux-arm64" => "ai"
       end
     else
-      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.1/ai-linux-amd64"
-      sha256 "83dabd0044de544a390c90e8f0a88c296663e5e9312db49a191f4756196d0bb3"
+      url "https://github.com/convergent-systems-co/ai/releases/download/v0.4.0/ai-linux-amd64"
+      sha256 "922beb89fae52ced42505f72378cd1c4be183bf5aea5989eb5c0f835ba7aebb1"
 
       def install
         bin.install "ai-linux-amd64" => "ai"
