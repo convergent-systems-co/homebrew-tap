665fd745ff5e1c32f9b31872e1f9d8e1f0a2a832
Thomas Polliard <thomas@polliard.com>
2026-09-10 10:28:04 -0500

    chore: update thread to v0.1.3

--- Formula/thread.rb
+++ Formula/thread.rb
@@ -4,28 +4,28 @@
 class Thread < Formula
   desc "Obsidian-backed memory and resume CLI for AI-assisted work"
   homepage "https://github.com/convergent-systems-co/thread"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_darwin_arm64.tar.gz"
-      sha256 "53a4473312e516a94c854af8140ec3ca06409d13eccb8afa1d386f65158a30ba"
+      sha256 "3723191af2f155afa185621d7be988e0b32334d636135c7dea5dd85e80e7a323"
     end
     on_intel do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_darwin_amd64.tar.gz"
-      sha256 "443e06002a15392a11195b89239442a9c9f1202e632d7d118a8d2820f0ecf9ac"
+      sha256 "5265196ca6dc1a04321c163bb0bacbf393c06d15288eb6aa91b9e298266c88a8"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_linux_arm64.tar.gz"
-      sha256 "e14b81af3453f0f9bbe5b15bfef4783f502f09c0412668f1a3327010b391e177"
+      sha256 "715d75f68a26b5d865e5da4dbc1f92800a42fe815a089b9f4e885fde22fe6c11"
     end
     on_intel do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_linux_amd64.tar.gz"
-      sha256 "f1583619ff75ea4b32c672573888d0e694efcd50ab53a3fa5eaaa55377aa8080"
+      sha256 "117075a3d3f78994667a8394de9c095f67df42c04f0c6336230bd7876ccd4146"
     end
   end
 
