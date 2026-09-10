32777207a8422d07dca5d611fb201fc4e4172a2a
Thomas Polliard <thomas@polliard.com>
2026-09-09 20:52:49 -0500

    thread 0.1.2: native hooks and canonical hidden storage

--- Formula/thread.rb
+++ Formula/thread.rb
@@ -4,28 +4,28 @@
 class Thread < Formula
   desc "Obsidian-backed memory and resume CLI for AI-assisted work"
   homepage "https://github.com/convergent-systems-co/thread"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_darwin_arm64.tar.gz"
-      sha256 "1152a7a1df1cc5982a87c5daaf465e2f1956e01e01cbbef20bcb117330a383b6"
+      sha256 "53a4473312e516a94c854af8140ec3ca06409d13eccb8afa1d386f65158a30ba"
     end
     on_intel do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_darwin_amd64.tar.gz"
-      sha256 "9983e335227f3f46dcacf41491a17eca46129881e445e353b1ad38366b0837cd"
+      sha256 "443e06002a15392a11195b89239442a9c9f1202e632d7d118a8d2820f0ecf9ac"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_linux_arm64.tar.gz"
-      sha256 "689e61e5f082f2c50ea3c10432e961a01958d39730c439715844dc7a5875431b"
+      sha256 "e14b81af3453f0f9bbe5b15bfef4783f502f09c0412668f1a3327010b391e177"
     end
     on_intel do
       url "https://github.com/convergent-systems-co/thread/releases/download/v#{version}/thread_#{version}_linux_amd64.tar.gz"
-      sha256 "7ec1429224a081aaf55eb3d78efd4f7df57a404c4ffa0c9e70347dfb43a1e09e"
+      sha256 "f1583619ff75ea4b32c672573888d0e694efcd50ab53a3fa5eaaa55377aa8080"
     end
   end
 
@@ -35,5 +35,8 @@ class Thread < Formula
 
   test do
     assert_equal version.to_s, shell_output("#{bin}/thread --version").strip
+    system bin/"thread", "--vault", testpath, "capture", "Storage location check"
+    assert_predicate testpath/"Thread/.items", :directory?
+    refute_predicate testpath/"Thread/Items", :exist?
   end
 end
